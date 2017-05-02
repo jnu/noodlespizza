ea8e9d7535502749163d9f68ba45ff86d167722a
Joe Nudell <joen@dropbox.com>
2017-05-02 02:11:02 -0700

    add ga

--- src/index.js
+++ src/index.js
@@ -3,6 +3,19 @@ import views from './views';
 import auth from './auth';
 
 
+/**
+ * Google analytics tracking code. Include it on every page.
+ */
+const googleAnalytics = `
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-98374296-1', 'auto');
+  ga('send', 'pageview');`;
+
+
 export default (dreija, env) => {
         dreija
             .title(`Noodles' Pizza`)
@@ -11,5 +24,6 @@ export default (dreija, env) => {
             .auth(auth)
             .dbname('noodlespizza')
             .dbhost(env.DBHOST)
-            .redishost(env.REDISHOST);
+            .redishost(env.REDISHOST)
+            .injectScript(googleAnalytics, true);
 };
